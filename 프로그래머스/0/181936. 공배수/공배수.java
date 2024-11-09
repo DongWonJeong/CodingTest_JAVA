df418147d778d5d9d45014c78787b8be78e1bf3e
DongWonJeong <88620992+DongWonJeong@users.noreply.github.com>
2024-11-09 19:18:01 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,10 +1,12 @@
 class Solution {
     public int solution(int number, int n, int m) {
         
-        if (number % n == 0 && number % m == 0){
-            return 1;
-        } else {
-            return 0;
-        }
+        if(number % n == 0){
+            if (number % m == 0){
+                return 1;
+            } 
+        } 
+        
+        return 0;
     }
 }
