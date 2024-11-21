355c2b016beeba66c8fc4e279ad1d78507d06599
DongWonJeong <88620992+DongWonJeong@users.noreply.github.com>
2024-11-21 18:24:17 +0900

    [level 0] Title: 접미사 배열, Time: 0.76 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        int length = my_string.length();
+        String[] arr = new String[length];
+
+        for(int i = 0; i < length; i++) {
+            arr[i] = my_string.substring(i);
+        }
+
+        Arrays.sort(arr);
+
+        return arr;
+    }
+}
